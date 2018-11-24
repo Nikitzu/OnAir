1e158884bc97f8190a2e966054e880a69514e32c
Nikitz <nikitzhuykov@gmail.com>
2018-11-24 12:27:47 +0300

    add get xml method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import requestUrl from './resources/OnAir_2018_NOV.xml'
 
 class App extends Component {
+  getXML = () => {
+    var oReq = new XMLHttpRequest();
+    oReq.onload = this.reqListener;
+    oReq.open("get", `${requestUrl}`, true);
+    oReq.send();
+  }
+  
+  reqListener(e) {
+    let parser = new DOMParser();
+    let xmlDoc = parser.parseFromString(this.responseText,"text/xml");
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +24,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <button onClick={this.getXML}>
+            Load Data
+          </button>
           <a
             className="App-link"
             href="https://reactjs.org"
